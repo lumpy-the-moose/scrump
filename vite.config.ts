e23e19b4e60e0da106e5863610edbc3769e1932e
lumpy-the-moose <vmaobo@gmail.com>
2023-01-06 00:22:06 +0200

    gh pages deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/scrump/`,
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   server: {
     port: 3000,
